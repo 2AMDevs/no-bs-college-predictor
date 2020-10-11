799d6e6068d7b566990beac9c9bc7b2f214d80cb
Mohit K. Yadav <mohitkyadav@outlook.com>
2020-10-11 20:38:18 +0530

    style table filter and add shadow

--- src/components/TableFilter/TableFilter.js
+++ src/components/TableFilter/TableFilter.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
+import './TableFilter.css';
+
 const TableFilter = ({ title, property, value, data, updateFilters }) => {
   return (
     <th>{title}
-      { data?.length && <select
-        id={property}
-        name={title}
-        value={value}
-        onChange={(e) => updateFilters(property, e.target.value)}
-      >
-        {data.map(d => (
-          <option key={d} value={d}>{d}</option>
-        ))}
-      </select> }
-  </th>
+      {data?.length && (
+        <select
+          id={property}
+          name={title}
+          value={value}
+          onChange={(e) => updateFilters(property, e.target.value)}
+          className="table-filter"
+        >
+          {data.map(d => (
+            <option key={d} value={d}>{d}</option>
+          ))}
+        </select>
+      )}
+    </th>
   )
 }
 
