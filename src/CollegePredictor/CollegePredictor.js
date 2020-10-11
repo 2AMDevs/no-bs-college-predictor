35e9d4da77abdc09de8894fc1b1d63638991f0e5
Mohit K. Yadav <mohitkyadav@outlook.com>
2020-10-11 12:14:39 +0530

    input

---
+++ src/CollegePredictor/CollegePredictor.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './CollegePredictor.css';
+
+function CollegePredictor() {
+  return (
+    <div className="college-predictor">
+    <input />
+    </div>
+  );
+}
+
+export default CollegePredictor;
